282635aae4872717f085faeb3f9b96f1dff9c380
nezlayaya <nezlayaya94@mail.ru>
2018-01-03 11:19:42 +0200

    ADD: autorization http

--- src/app/autorization/authorization.form.ts
+++ src/app/autorization/authorization.form.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-authorization',
@@ -7,9 +8,14 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class AuthorizationComponent implements OnInit {
-  constructor() { }
+  constructor(
+    public http: Http
+  ) { }
 
   ngOnInit() {
+    this.http.get('').subscribe(data => {
+      console.log(data);
+    });
   }
 
   onSubmit(form: any): void {
