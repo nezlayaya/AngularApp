b144731107d258cd786623fcc566282c19516528
nezlayaya <nezlayaya94@gmail.com>
2018-02-07 13:37:03 +0200

    FIXED:some bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {HttpModule} from "@angular/http";
 import { AppComponent } from './app.component';
 import {RegistrationComponent} from './registration/registration.form';
+import {AuthorizationComponent} from './autorization/authorization.form';
+import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {AuthorizationComponent} from './autorization/authorization.form';
 
 
 
--- src/app/autorization/authorization.form.ts
+++ src/app/autorization/authorization.form.ts
@@ -1,26 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {Http} from '@angular/http';
-import {Observable} from "rxjs/Observable";
+
 
 @Component({
   selector: 'app-authorization',
-  templateUrl: 'authorization.html',
-  styleUrls: ['authorization.css']
+  templateUrl: 'authorization-form.html',
+  styleUrls: ['authorization-form.css']
 })
 
 export class AuthorizationComponent implements OnInit {
-  constructor(public http: Http) {
-  }
-
-  getJSON() {
-    this.http.get('./names.json')
-      .subscribe((res: any) => {
-        console.log(res);
-      });
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.getJSON();
   }
 
   onSubmit(form: any): void {
